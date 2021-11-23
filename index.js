5ffe20c6e55cc03daa101fdd6e5dc1c1e62538db
Thạch Nguyễn <nnthach1024@gmail.com>
2021-11-23 22:01:25 +0700

    add topic

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var addItem = document.querySelector('.add-item');
 addItem.querySelector('#add-button').addEventListener("click", submit);
 
+
 var idInput = document.getElementById('id-form');
 idInput.addEventListener("keyup", (e) => {
     if (e.keyCode === 13) {
@@ -17,33 +18,118 @@ authorInput.addEventListener('keyup', (e) => {
     }
 })
 
+
 var titleInput = document.getElementById('title-form');
 titleInput.addEventListener('keyup', (e) => {
     if (e.keyCode === 13) {
         e.preventDefault();
-        idInput.focus();
+        topicTitleInput.focus();
+
     }
 })
 
+var topicTitleInput = document.getElementById('topic-title-form');
+topicTitleInput.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        topicDescInput.focus();
+    }
+})
+var topicDescInput = document.getElementById('topic-desc-form');
+topicDescInput.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        idInput.focus();
+        topicCreate.click();
+    }
+})
+
+var topicCreate = document.getElementById('topic-create-button');
+topicCreate.addEventListener('click', createTopic);
+function createTopic() {
+    let tempTopicTitle = topicTitleInput.value;
+    if (tempTopicTitle == '') {alert('Chưa nhập chủ đề!!!'); return;}
+    for (let i in topicList) {
+        if (topicList[i].title == tempTopicTitle) {alert(`Chủ đề ${tempTopicTitle} đã tồn tại!!!`); return;}
+    }
+    let title = manageItem.querySelector(".badge");
+    title.innerHTML = tempTopicTitle;
+    topicTitleInput.value = '';
+    topicDescInput.value = '';
+}
 
 
+var topicList = [];
+
+var topicAdd = document.getElementById('add-topic-button');
+topicAdd.addEventListener('click', addTopic);
+function addTopic() {
+    addTopicToList();
+}
+
+function addTopicToList() {
+    if (itemList.length == 0) {alert('Chưa có thẻ nào được thêm!!!'); return;}
+    topic = {
+        title: manageItem.querySelector(".badge").innerHTML,
+        desc: topicDescInput.value,
+        items: itemList.map(e => e.id)
+    }
+    topicList.push(topic);
+    itemList = [];
+    clearItemList();
+
+    console.log(topicList);
+    manageItem.querySelector(".badge").innerHTML = "Quản lý thẻ";
+    var li = document.createElement('li');
+    li.className = 'list-group-item topic-li'; 
+    li.innerHTML = `<div class="topic-card">
+    <i class="fas fa-arrows-alt"></i>
+    <span class = "badge text-dark topic-id">${topic.title}</span>
+    <button class="btn btn-danger btn-sm" type="button" class="delete" onclick="deleteTopicFromList(this)">Xoá</button>   
+    </div>`;
+    document.querySelector('.topic-list-group').appendChild(li);
+    updateTopicList(); 
+}
+
+
+function updateTopicList() {
+    let cloneTopicList = [];
+    var orderedList = document.getElementsByClassName('topic-li');
+    for (var i = 0; i < orderedList.length; i++) {
+        var tempTitle = orderedList[i].querySelector(".topic-id").innerHTML;
+        console.log(tempTitle);
+        for (j in topicList) {
+            if (topicList[j].title == tempTitle) {
+                cloneTopicList.push(topicList[j]);
+                break;
+            }
+        }
+    }
+    topicList = cloneTopicList;
+}
+
+function deleteTopicFromList(e) {
+    cloneTopicList = [...topicList];
+    topicList = cloneTopicList.filter( function(topic) {
+        return topic.title != e.parentElement.querySelector('#topic-id').innerHTML;
+    })
+    e.parentElement.parentElement.remove();
+    updateTopicList();
+}
+
+/*Xy ly item them vao */
 class Item {
     constructor() {
         this.id = 0;
     }
 }
 
-
-
-
-
 function submit() {
-    var numberRegex = new RegExp('[^0-9]');
     let tempId = addItem.querySelector('#id-form').value;
+    if (manageItem.querySelector(".badge").innerHTML == "Quản lý thẻ") {alert('Chưa có chủ đề!!!'); return;}
     if (tempId == '') {alert('Chưa nhập ID!!!'); return;}
     let tempIdList = getListNumberFromString(tempId);
     for (id in tempIdList) {
-        console.log("Id: ",id);
         tempItem = new Item();
         tempItem.id = parseInt(tempIdList[id]);
         addItemToList(tempItem);
@@ -55,27 +141,26 @@ function addItemToList(item) {
     if (itemList.some((i) => {
         return i.id == item.id;
     })) {
-        let alertt = `Thẻ "${item.id}" đã tồn tại.`
-        alert(alertt);
+        alert(`Thẻ "${item.id}" đã tồn tại.`);
         return;
     }
     itemList.push(item);
     var li = document.createElement('li');
-    li.className = 'list-group-item'; 
+    li.className = 'list-group-item item-li'; 
     li.innerHTML = `<div class="item-card">
     <i class="fas fa-arrows-alt"></i>
     <span class = "badge text-dark" id="item-id">${item.id}</span>
     <button class="btn btn-danger btn-sm" type="button" id="delete" onclick="deleteItemFromList(this)">Xoá</button>   
     </div>`;
     manageItem.querySelector('.list-group').appendChild(li);
-     
     updateItemList(); 
 }
 
 
 function updateItemList() {
     itemsClone = [];
-    var orderedList = manageItem.getElementsByClassName('list-group-item');
+    var orderedList = manageItem.getElementsByClassName('item-li');
+    console.log(orderedList);
     for (var i = 0; i < orderedList.length; i++) {
         var tempId = orderedList[i].querySelector("#item-id").innerHTML;
         tempId = parseInt(tempId);
@@ -98,6 +183,13 @@ function deleteItemFromList(e) {
     updateItemList();
 }
 
+function clearItemList() {
+    let ul = document.querySelector('.item-list ul');
+    while(ul.firstChild) {
+        ul.removeChild(ul.firstChild);
+    }
+}
+
 var itemList = [];
 var cloneItemList = [];
 
@@ -105,8 +197,10 @@ var manageItem = document.querySelector('.manage-item');
 manageItem.querySelector('.item-list').addEventListener("change", updateItemList);
 
 function createTrealet() {
-    if (itemList.length == 0) {
-        alert('Chưa có thẻ nào được thêm!!!'); 
+    updateTopicList();
+    console.log(topicList);
+    if (topicList.length == 0) {
+        alert('Chưa có chủ đề nào được thêm!!!'); 
         return;
     }
     let general = document.querySelector('.general');
@@ -119,9 +213,9 @@ function createTrealet() {
         exec: "streamline",
         title : _title,
         author : _author,
-        desc : general.querySelector('#description-form').value,
-        items : itemList.map( (e) => {
-            return e.id;
+        desc : '',
+        topic : topicList.map( (e) => {
+            return e;
         })
     }
     const jsonData = {
@@ -135,13 +229,6 @@ function createTrealet() {
 
 }
 
-manageItem.querySelector('#create-trealet-button').addEventListener('click', () => {
-
-    $('.modal fade').modal({
-        show: true
-    });
-
-})
 
 function download(content, fileName, contentType) {
     const a = document.createElement("a");
@@ -153,11 +240,9 @@ function download(content, fileName, contentType) {
 
 function getListNumberFromString(str) { 
     let num = str.replace(/[^0-9]/g, ' '); 
-    console.log(num);
     let i = 0;
     num = num.split('');
     while (i < num.length - 1) {
-        console.log(num, "len: ", num.length);
         if (num[i] == ' ' && num[i+1] == ' ') {
             num.splice(i, 1);
             continue;
@@ -166,6 +251,5 @@ function getListNumberFromString(str) {
     }
     num = num.join('');
     num = num.split(' ');
-    console.log(num);
     return num;
 }
